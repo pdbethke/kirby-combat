79f5ebc30eeb4ebf4a671b619393b07d31440104
Peter D Bethke <pdbethke@siteshell.net>
2026-04-25 23:37:43 -0400

    feat(vehicles): add Ramming (extreme move-through)
    DC = round(SIZE * velocity_m_per_segment / 12); +1 DC at >60 m/segment.
    Attacker takes half DC self-damage like Move Through. Vehicle BODY
    decremented by 1 per ram.
    
    Citation: 6E Vehicles supplement p33 (Ramming).

--- kirby_combat/vehicles/__init__.py
+++ kirby_combat/vehicles/__init__.py
@@ -7,10 +7,14 @@ from kirby_combat.vehicles.passenger import (
     apply_shared_fate, passenger_can_be_rescued,
     SharedFateResult,
 )
+from kirby_combat.vehicles.ramming import (
+    RammingResult, resolve_ramming, ramming_dc,
+)
 
 __all__ = [
     "Vehicle", "Passenger", "max_passengers_for_size",
     "passenger_cover_level", "can_attack_out",
     "apply_shared_fate", "passenger_can_be_rescued",
     "SharedFateResult",
+    "RammingResult", "resolve_ramming", "ramming_dc",
 ]
